fdac7bf3124b754459802be150a238c173d07deb
Bruksin <smirnov.pa@phystech.edu>
2022-02-21 15:00:16 +0300

    Can bachup files now

--- solution.sh
+++ solution.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 6 строчку написал, найдя часть инфы на стакоферфлоу
 work_done='done'
-mkdir $1
+mkdir $3
+find $1 -name "*.$2" -exec cp —backup=numbered {} ./$3 \;
 echo $work_done
